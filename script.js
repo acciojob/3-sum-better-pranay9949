8275e052f7fd6920b5540ff92f2bdab4aa087441
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-09 11:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
 // write your code here
+	let sum=0;
+	for(let i=0;i<arr.length-2;i++){
+		
+		for(let j=i+1;j<length-1;j++){
+			for(let k=i+2;k<length;k++){
+				
+			   sum=arr[i]+arr[j]+arr[k];
+				let final=Math.abs(target-sum);
+				if(sum>final){
+					sum=final;
+				}
+				
+			}
+		}
+	}
+	return sum;
   
 }
 
