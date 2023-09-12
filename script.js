ffd01ab66eca6a60ff25adcf7cd67ce0f234f119
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-12 14:47:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 
 function threeSum(arr, target) {
 // write your code here
-	let result=Number.MAX_SAFE_INTEGER;
-	let final_sum;
-	for(let i=0;i<arr.length-2;i++){
-		
-		for(let j=i+1;j<arr.length-1;j++){
-			for(let k=i+2;k<arr.length;k++){
-				
-			   let sum=arr[i]+arr[j]+arr[k];
-				let final=Math.abs(target-sum);
-				if(result>final){
-					result=final;
-					final_sum=sum;
-				
-			}
-		}
-	}
-	return final_sum;
-  
+	arr.sort((a, b) => a - b);
+
+    let closestSum = Number.MAX_SAFE_INTEGER;
+    let closestTriplet;
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        let left = i + 1;
+        let right = arr.length - 1;
+
+        while (left < right) {
+            const sum = arr[i] + arr[left] + arr[right];
+            const diff = Math.abs(target - sum);
+
+            if (diff < closestSum) {
+                closestSum = diff;
+                closestTriplet = sum;
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestTriplet
 }
 
 
